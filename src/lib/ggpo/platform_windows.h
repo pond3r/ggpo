9d024aa219dbc81db09b782c256f78863e76a3b6
Silvan Jegen <s.jegen@gmail.com>
2020-01-02 19:43:32 +0100

    ggpo/platform_windows: fix compilation

--- src/lib/ggpo/platform_windows.h
+++ src/lib/ggpo/platform_windows.h
@@ -12,6 +12,7 @@
 #include <WS2tcpip.h>
 #include <windows.h>
 #include <stdio.h>
+#include <stdint.h>
 
 class Platform {
 public:  // types
@@ -20,7 +21,7 @@ public:  // types
 public:  // functions
    static ProcessID GetProcessID() { return GetCurrentProcessId(); }
    static void AssertFailed(char *msg) { MessageBoxA(NULL, msg, "GGPO Assertion Failed", MB_OK | MB_ICONEXCLAMATION); }
-   static uint32 GetCurrentTimeMS() { return timeGetTime(); }
+   static uint32_t GetCurrentTimeMS() { return timeGetTime(); }
    static void SleepMS(int ms) { Sleep(ms); }
    static void CreateDirectory(const char* pathname, const void* junk) { CreateDirectoryA(pathname, junk); }
 };

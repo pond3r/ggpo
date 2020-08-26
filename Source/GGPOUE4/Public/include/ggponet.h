cbbe32e2748ee5c22ab66295dfe8469741648845
BwdYeti <BwdYeti@gmail.com>
2020-08-26 16:45:35 -0400

    GGPOSessionCallbacks
    -Replace GGPOSessionCallbacks function pointers with std::function if compiling with C++11 or higher; mainly allows using member functions for callbacks, not just free functions

--- Source/GGPOUE4/Public/include/ggponet.h
+++ Source/GGPOUE4/Public/include/ggponet.h
@@ -261,65 +261,137 @@ typedef struct {
    } u;
 } GGPOEvent;
 
+
+// If C++11, use std::function instead of function pointers for GGPOSessionCallbacks
+#if __cplusplus >= 201103L || (defined(_MSC_VER) && _MSC_VER >= 1900)
+
+// Close extern "C"
+};
+
 /*
  * The GGPOSessionCallbacks structure contains the callback functions that
  * your application must implement.  GGPO.net will periodically call these
  * functions during the game.  All callback functions must be implemented.
  */
-typedef struct {
-   /*
-    * begin_game callback - This callback has been deprecated.  You must
-    * implement it, but should ignore the 'game' parameter.
-    */
-   bool (__cdecl *begin_game)(const char *game);
+struct GGPOSessionCallbacks {
+    /*
+     * begin_game callback - This callback has been deprecated.  You must
+     * implement it, but should ignore the 'game' parameter.
+     */
+    std::function<bool(const char* game)> begin_game;
 
-   /*
-    * save_game_state - The client should allocate a buffer, copy the
-    * entire contents of the current game state into it, and copy the
-    * length into the *len parameter.  Optionally, the client can compute
-    * a checksum of the data and store it in the *checksum argument.
-    */
-   bool (__cdecl *save_game_state)(unsigned char **buffer, int *len, int *checksum, int frame);
+    /*
+     * save_game_state - The client should allocate a buffer, copy the
+     * entire contents of the current game state into it, and copy the
+     * length into the *len parameter.  Optionally, the client can compute
+     * a checksum of the data and store it in the *checksum argument.
+     */
+    std::function<bool(unsigned char** buffer, int* len, int* checksum, int frame)> save_game_state;
 
-   /*
-    * load_game_state - GGPO.net will call this function at the beginning
-    * of a rollback.  The buffer and len parameters contain a previously
-    * saved state returned from the save_game_state function.  The client
-    * should make the current game state match the state contained in the
-    * buffer.
-    */
-   bool (__cdecl *load_game_state)(unsigned char *buffer, int len);
+    /*
+     * load_game_state - GGPO.net will call this function at the beginning
+     * of a rollback.  The buffer and len parameters contain a previously
+     * saved state returned from the save_game_state function.  The client
+     * should make the current game state match the state contained in the
+     * buffer.
+     */
+    std::function<bool(unsigned char* buffer, int len)> load_game_state;
 
-   /*
-    * log_game_state - Used in diagnostic testing.  The client should use
-    * the ggpo_log function to write the contents of the specified save
-    * state in a human readible form.
-    */
-   bool (__cdecl *log_game_state)(char *filename, unsigned char *buffer, int len);
+    /*
+     * log_game_state - Used in diagnostic testing.  The client should use
+     * the ggpo_log function to write the contents of the specified save
+     * state in a human readible form.
+     */
+    std::function<bool(char* filename, unsigned char* buffer, int len)> log_game_state;
 
-   /*
-    * free_buffer - Frees a game state allocated in save_game_state.  You
-    * should deallocate the memory contained in the buffer.
-    */
-   void (__cdecl *free_buffer)(void *buffer);
+    /*
+     * free_buffer - Frees a game state allocated in save_game_state.  You
+     * should deallocate the memory contained in the buffer.
+     */
+    std::function<void(void* buffer)> free_buffer;
 
-   /*
-    * advance_frame - Called during a rollback.  You should advance your game
-    * state by exactly one frame.  Before each frame, call ggpo_synchronize_input
-    * to retrieve the inputs you should use for that frame.  After each frame,
-    * you should call ggpo_advance_frame to notify GGPO.net that you're
-    * finished.
-    *
-    * The flags parameter is reserved.  It can safely be ignored at this time.
-    */
-   bool (__cdecl *advance_frame)(int flags);
+    /*
+     * advance_frame - Called during a rollback.  You should advance your game
+     * state by exactly one frame.  Before each frame, call ggpo_synchronize_input
+     * to retrieve the inputs you should use for that frame.  After each frame,
+     * you should call ggpo_advance_frame to notify GGPO.net that you're
+     * finished.
+     *
+     * The flags parameter is reserved.  It can safely be ignored at this time.
+     */
+    std::function<bool(int flags)> advance_frame;
 
-   /* 
-    * on_event - Notification that something has happened.  See the GGPOEventCode
-    * structure above for more information.
-    */
-   bool (__cdecl *on_event)(GGPOEvent *info);
-} GGPOSessionCallbacks;
+    /*
+     * on_event - Notification that something has happened.  See the GGPOEventCode
+     * structure above for more information.
+     */
+    std::function<bool(GGPOEvent * info)> on_event;
+};
+
+extern "C" {
+#else
+
+/*
+ * The GGPOSessionCallbacks structure contains the callback functions that
+ * your application must implement.  GGPO.net will periodically call these
+ * functions during the game.  All callback functions must be implemented.
+ */
+ typedef struct {
+     /*
+      * begin_game callback - This callback has been deprecated.  You must
+      * implement it, but should ignore the 'game' parameter.
+      */
+     bool(__cdecl* begin_game)(const char* game);
+
+     /*
+      * save_game_state - The client should allocate a buffer, copy the
+      * entire contents of the current game state into it, and copy the
+      * length into the *len parameter.  Optionally, the client can compute
+      * a checksum of the data and store it in the *checksum argument.
+      */
+     bool(__cdecl* save_game_state)(unsigned char** buffer, int* len, int* checksum, int frame);
+
+     /*
+      * load_game_state - GGPO.net will call this function at the beginning
+      * of a rollback.  The buffer and len parameters contain a previously
+      * saved state returned from the save_game_state function.  The client
+      * should make the current game state match the state contained in the
+      * buffer.
+      */
+     bool(__cdecl* load_game_state)(unsigned char* buffer, int len);
+
+     /*
+      * log_game_state - Used in diagnostic testing.  The client should use
+      * the ggpo_log function to write the contents of the specified save
+      * state in a human readible form.
+      */
+     bool(__cdecl* log_game_state)(char* filename, unsigned char* buffer, int len);
+
+     /*
+      * free_buffer - Frees a game state allocated in save_game_state.  You
+      * should deallocate the memory contained in the buffer.
+      */
+     void(__cdecl* free_buffer)(void* buffer);
+
+     /*
+      * advance_frame - Called during a rollback.  You should advance your game
+      * state by exactly one frame.  Before each frame, call ggpo_synchronize_input
+      * to retrieve the inputs you should use for that frame.  After each frame,
+      * you should call ggpo_advance_frame to notify GGPO.net that you're
+      * finished.
+      *
+      * The flags parameter is reserved.  It can safely be ignored at this time.
+      */
+     bool(__cdecl* advance_frame)(int flags);
+
+     /*
+      * on_event - Notification that something has happened.  See the GGPOEventCode
+      * structure above for more information.
+      */
+     bool(__cdecl* on_event)(GGPOEvent* info);
+ } GGPOSessionCallbacks;
+
+#endif
 
 
 class GGPOUE4_API GGPONet
